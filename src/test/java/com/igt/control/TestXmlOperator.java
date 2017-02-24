49a403f0ba53f51a639d6b899c80c1d9140d8297
zhuanghua <tonyzhuanghua@gmail.com>
2017-02-24 23:07:03 +0000

    add test method

--- src/test/java/com/igt/control/TestXmlOperator.java
+++ src/test/java/com/igt/control/TestXmlOperator.java
@@ -5,17 +5,18 @@ import com.igt.control.xml.ParseXml;
 import com.igt.entity.Avvenimento;
 import com.igt.control.sort.ByCodProgramaDesc;
 import com.igt.control.xml.ParseXmlImpl;
+import com.sun.deploy.util.StringUtils;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by zhuanghua on 2017/2/24.
  */
 public class TestXmlOperator {
 
-   private List<Avvenimento> avvenimentos;
+    private List<Avvenimento> avvenimentos;
 
     @Before
     public void parse_xml() throws Exception {
@@ -33,10 +34,10 @@ public class TestXmlOperator {
         for (Avvenimento avvenimento : out) {
             System.out.println(
                     avvenimento.getCodPrograma() + "," +
-                    avvenimento.getSiglaSport() + "," +
-                    avvenimento.getDesManif() + "," +
-                    avvenimento.getDesAvvenimento() + "," +
-                    avvenimento.getDateTime()
+                            avvenimento.getSiglaSport() + "," +
+                            avvenimento.getDesManif() + "," +
+                            avvenimento.getDesAvvenimento() + "," +
+                            avvenimento.getDateTime()
             );
         }
     }
@@ -59,5 +60,38 @@ public class TestXmlOperator {
                             avvenimento.getDateTime()
             );
         }
+    }
+
+    @Test
+    public void a_list_of_all_the_different_types_of_des_tipo_sco_divided_by_sigla_sport() throws Exception {
+        Map<String, Set<String>> map = new HashMap<String, Set<String>>();
+        for (Avvenimento avvenimento : avvenimentos) {
+            String siglaSport = avvenimento.getSiglaSport();
+
+            Set<String> set = map.get(siglaSport);
+
+            if (set == null) {
+                set = new HashSet<String>();
+            }
+            List<String> desTipoScos = avvenimento.getDesTipoSco();
+
+            for (String desTipoSco : desTipoScos) {
+                set.add(desTipoSco);
+            }
+
+            map.put(siglaSport, set);
+        }
+
+        Iterator it = map.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry entry = (Map.Entry) it.next();
+            String key = (String) entry.getKey();
+            Set<String> val = (HashSet<String>) entry.getValue();
+            String a = StringUtils.join(val, ", ");
+
+            System.out.println("sigla_sport =" + key + ":");
+            System.out.println(a);
+        }
+
     }
 }

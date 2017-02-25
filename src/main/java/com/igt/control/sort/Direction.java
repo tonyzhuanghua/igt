562a04c89e421fd02997610309504d5dfbc9c029
zhuanghua <tonyzhuanghua@gmail.com>
2017-02-25 12:25:32 +0000

    refine

--- src/main/java/com/igt/control/sort/Direction.java
+++ src/main/java/com/igt/control/sort/Direction.java
@@ -1,16 +1,16 @@
 package com.igt.control.sort;
 
 public enum Direction {
-    
+
     ASCENDING(-1), DESCENDING(1);
-    
+
     private int direction;
-    
-    Direction(int direction){
+
+    Direction(int direction) {
         this.direction = direction;
     }
-    
-    public int getDirection(){
+
+    public int getDirection() {
         return direction;
     }
 
